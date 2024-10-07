dd0f0d4bbb4c1b1cac2e1e6cb38726ae35ab860a
Elias Aggergaard Larsen <elias@takefive.dk>
2024-10-07 23:31:06 +0200

    Api can now parse Exercise object

--- src/main/java/Group15/Api/Exercise.java
+++ src/main/java/Group15/Api/Exercise.java
@@ -2,50 +2,14 @@ package Group15.Api;
 
 public class Exercise
 {
-    private final String title;
-    private final String description;
-    private final String type;
-    private final String bodyPart;
-    private final String equipment;
-    private final String difficulty;
+    public String title;
+    public String description;
+    public String type;
+    public String bodyPart;
+    public String equipment;
+    public String difficulty;
 
-    public Exercise(String title, String description, String type, String bodyPart, String equipment, String difficulty)
+    public Exercise()
     {
-        this.title = title;
-        this.description = description;
-        this.type = type;
-        this.bodyPart = bodyPart;
-        this.equipment = equipment;
-        this.difficulty = difficulty;
-    }
-
-    public String getTitle()
-    {
-        return title;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public String getType()
-    {
-        return type;
-    }
-
-    public String getBodyPart()
-    {
-        return bodyPart;
-    }
-
-    public String getEquipment()
-    {
-        return equipment;
-    }
-
-    public String getDifficulty()
-    {
-        return difficulty;
     }
 }
