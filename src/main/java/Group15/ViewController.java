6981b34813e872ab276cb4efe022b9386858da64
Mustafa <s235092@dtu.dk>
2024-10-09 19:42:18 +0200

    Maximum 5 buttons can be selected, and a submit button prints the selected the muscles in all caps.

--- src/main/java/Group15/ViewController.java
+++ src/main/java/Group15/ViewController.java
@@ -2,21 +2,26 @@ package Group15;
 
 import Group15.Api.BodyPart;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ViewController
 {
     private static Stage stage;
+    private static final int MAX_BUTTON_SELECTION = 5;
 
     public static void init(Stage stage)
     {
@@ -36,17 +41,38 @@ public class ViewController
     }
 
     public static Scene createMuscleSelector() {
-        VBox vbox = new VBox();
-        Scene scene = new Scene(vbox,650,650);
-        for(BodyPart bodyPart : BodyPart.values()) {
-            ToggleButton btn = new ToggleButton(bodyPart.toString());
-            btn.setOnAction(e -> {
-                System.out.println("I am body part: " + bodyPart.toString());
+        VBox vBox = new VBox();
+        List<ToggleButton> toggleButtons = new ArrayList<>();
+
+        for (BodyPart bodyPart : BodyPart.values()) {
+            ToggleButton toggleButton = new ToggleButton(bodyPart.toString());
+            toggleButtons.add(toggleButton);
+            vBox.getChildren().add(toggleButton);
+
+            toggleButton.setOnAction(e -> {
+                long amountSelected = toggleButtons.stream().filter(ToggleButton::isSelected).count();
+
+                if(amountSelected > MAX_BUTTON_SELECTION) {
+                    toggleButton.setSelected(false);
+                    System.out.println("You have exceeded max selection: " + MAX_BUTTON_SELECTION);
+                }
             });
-            vbox.getChildren().add(btn);
         }
 
+        Button submitButton = new Button("Submit");
+        VBox.setMargin(submitButton, new Insets(50, 0, 0, 0));
+        vBox.getChildren().add(submitButton);
 
-        return scene;
+        submitButton.setOnAction(e -> {
+            List<String> selectedButtonNames = new ArrayList<>();
+            for (ToggleButton toggleButton : toggleButtons) {
+                if (toggleButton.isSelected()) {
+                    selectedButtonNames.add(toggleButton.getText().toUpperCase());
+                }
+            }
+            System.out.println("Selected buttons: " + String.join(", ", selectedButtonNames));
+        });
+
+        return new Scene(vBox, 600,600);
     }
 }
