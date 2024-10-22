083cdaed3d7af401a27124965eff12037c5645af
Mustafa Baker <s235092@dtu.dk>
2024-10-22 10:07:13 +0200

    Created a WorkoutAlgorithm class

---
+++ src/main/java/Group15/WorkoutAlgorithm.java
@@ -0,0 +1,13 @@
+package Group15;
+
+import Group15.Api.Exercise;
+
+import java.util.List;
+
+public class WorkoutAlgorithm {
+
+    //TODO: Should return a workout, when class is implemented
+    public static void createWorkoutFromExercises(List<Exercise> exercises, int workoutTime) {
+
+    }
+}
