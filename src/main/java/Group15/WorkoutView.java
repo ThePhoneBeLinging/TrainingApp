635486df160cb7c3b4ba698c8d3659c3f4f510e0
Mads Hartwich <s235102@dtu.dk>
2024-10-24 14:45:04 +0200

    created skeleton for workoutView

--- src/main/java/Group15/WorkoutView.java
+++ src/main/java/Group15/WorkoutView.java
@@ -1,8 +1,8 @@
 package Group15;
 
-import Group15.Api.Exercise; //Should be replaced with Workout
+import Group15.Api.Exercise;
 
-
+import ch.qos.logback.core.Layout;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,12 +15,70 @@ import javafx.scene.paint.Color;
 
 public class WorkoutView {
 
-    //Exercise have been used to test the view, should be replaced with workout
+    private static String title = "Workout";
+    private static String[] buttons = {"Back", "Edit Workout", "Save As PDF"};
     public static Scene createScene(){
         VBox layout = new VBox();
         layout.setSpacing(20);
         layout.setAlignment(Pos.CENTER);
 
+        Pane titlePane = createTitlePane();
+        layout.getChildren().add(titlePane);
+
+        Pane WorkoutPane = createWorkoutPane();
+        layout.getChildren().add(WorkoutPane);
+
+        Pane buttonPane = createButtonPane();
+        layout.getChildren().add(buttonPane);
+
         return new Scene(layout, 1000, 800);
     }
+
+    private static Pane createTitlePane(){
+        HBox titlePane = new HBox();
+        titlePane.setAlignment(Pos.TOP_CENTER);
+        Label titleLabel = new Label(title);
+        titleLabel.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+        titlePane.getChildren().add(titleLabel);
+
+        return titlePane;
+    }
+
+    private static Pane createWorkoutPane(){
+        VBox workoutPane = new VBox();
+        workoutPane.setAlignment(Pos.CENTER);
+        workoutPane.setSpacing(20);
+        workoutPane.setPrefSize(640, 600);
+        workoutPane.setMaxWidth(Region.USE_PREF_SIZE);
+
+        workoutPane.setBackground(new Background(new BackgroundFill(Color.GREY, null, null)));
+        Label workoutLabel = new Label("A list of exercises from the given workout");
+
+        workoutPane.getChildren().add(workoutLabel);
+
+        return workoutPane;
+    }
+
+    private static Pane createButtonPane(){
+        HBox buttonPane = new HBox();
+        buttonPane.setAlignment(Pos.CENTER);
+
+        for (String button : buttons){
+            Button newButton = new Button(button);
+            newButton.setPrefSize(200, 50);
+            newButton.setOnAction(_ -> {
+                switch (button){
+                    case "Back" -> ViewController.setScene(HomeScreenView.createScene());
+                    case "Edit Workout" -> System.out.println("Edit Workout Pressed");
+                    case "Save As PDF" -> System.out.println("Save As PDF Pressed");
+                }
+            });
+
+            buttonPane.getChildren().add(newButton);
+            buttonPane.setSpacing(20);
+        }
+
+        return buttonPane;
+    }
+
 }
