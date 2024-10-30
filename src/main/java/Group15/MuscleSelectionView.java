cfad37b66150b46098b52a769bc660e0283f658f
Mustafa Baker <s235092@dtu.dk>
2024-10-30 12:32:31 +0100

    Moved code outside of the scene creation for better readabilty (i hope). There should also be functionality to handle multiple equipment

--- src/main/java/Group15/MuscleSelectionView.java
+++ src/main/java/Group15/MuscleSelectionView.java
@@ -9,7 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
@@ -19,117 +18,153 @@ import java.util.Objects;
 public class MuscleSelectionView {
 
     public static Scene createMuscleSelectorScene() {
-        VBox mainVBox = new VBox();
-        HBox inputAndEquipBox = new HBox();
-        HBox imageAndTextButtons;
+        VBox mainVBox = new VBox(20);
+        mainVBox.setAlignment(Pos.CENTER);
+
+        HBox inputAndEquipBox = new HBox(20);
+        inputAndEquipBox.setAlignment(Pos.CENTER);
 
         List<String> selectedBodyParts = new ArrayList<>();
         List<String> dislikedBodyParts = new ArrayList<>();
         List<String> selectedEquipment = new ArrayList<>();
 
-        TilePane equipmentSelectorPane = new TilePane();
-        equipmentSelectorPane.setHgap(10);
-        equipmentSelectorPane.setVgap(10);
+        TextField minutesInputField = new TextField();
+        minutesInputField.setPrefSize(200, 50);
+        minutesInputField.setPromptText("Input how many minutes to workout");
+
+        GridPane equipmentSelectorGridPane = createEquipmentSelectorGridPane(selectedEquipment);
+
+        inputAndEquipBox.getChildren().addAll(minutesInputField, equipmentSelectorGridPane);
+
+        GridPane bodyPartsGridPane = createBodyPartsSelectorGridPane(selectedBodyParts);
+
+        Button submitButton = new Button("Submit");
+        submitButton.setPrefSize(200, 50);
+        submitButton.setOnAction(_ -> createSubmitButtonFunctionality(selectedBodyParts, dislikedBodyParts, selectedEquipment, minutesInputField));
+
+        Button backButton = new Button("Back");
+        backButton.setPrefSize(200, 50);
+        backButton.setOnAction(_ -> ViewController.setScene(HomeScreenView.createScene()));
+
+        mainVBox.getChildren().addAll(bodyPartsGridPane, inputAndEquipBox, submitButton, backButton);
+        VBox.setMargin(submitButton, new Insets(50, 0, 0, 0));
+
+        return new Scene(mainVBox, 1000, 800);
+    }
+
+    private static GridPane createEquipmentSelectorGridPane(List<String> selectedEquipment) {
+        GridPane equipmentSelectorGridPane = new GridPane();
+        equipmentSelectorGridPane.setVgap(10);
+        equipmentSelectorGridPane.setAlignment(Pos.CENTER);
+
         Label selectEquipmentText = new Label("Select Equipment");
         selectEquipmentText.setStyle("-fx-font-weight: bold");
+        equipmentSelectorGridPane.add(selectEquipmentText, 0, 0, 2, 1);
 
-        String[] equipments = {"All", "Body weight", "Barbell", "Dumbbell", "Machine"};
+        String[] equipments = {"Body weight", "Barbell", "Dumbbell", "Machine"};
+        List<CheckBox> equipmentCheckBoxes = new ArrayList<>();
 
-        equipmentSelectorPane.getChildren().add(selectEquipmentText);
+        CheckBox allCheckbox = new CheckBox("All");
+        allCheckbox.setSelected(true);
+        equipmentCheckBoxes.add(allCheckbox);
+        equipmentSelectorGridPane.add(allCheckbox, 0, 1, 2, 1);
 
-        for(int i = 0; i < equipments.length; i++) {
+        for (int i = 0; i < equipments.length; i++) {
             CheckBox equipmentCheckbox = new CheckBox(equipments[i]);
-            equipmentSelectorPane.getChildren().add(equipmentCheckbox);
-
-            if(equipmentCheckbox.isSelected()) {
-                if(equipmentCheckbox.getText().equals("All")) {
-                    equipmentCheckbox.setSelected(false);
-                }
-            }
+            equipmentCheckBoxes.add(equipmentCheckbox);
+            equipmentSelectorGridPane.add(equipmentCheckbox, i % 2, 2 + (i / 2));
         }
 
+        createEquipmentCheckboxFunctionality(allCheckbox, equipmentCheckBoxes, selectedEquipment);
+
+        return equipmentSelectorGridPane;
+    }
+
+    private static void createEquipmentCheckboxFunctionality(CheckBox allCheckbox, List<CheckBox> equipmentCheckBoxes, List<String> selectedEquipment) {
+        allCheckbox.setOnAction(_ -> {
+            if (allCheckbox.isSelected()) {
+                selectedEquipment.clear();
+                selectedEquipment.add("All");
+                for (CheckBox checkbox : equipmentCheckBoxes) {
+                    if (checkbox != allCheckbox) checkbox.setSelected(false);
+                }
+            }
+        });
+
+        for (CheckBox checkbox : equipmentCheckBoxes) {
+            if (checkbox != allCheckbox) {
+                checkbox.setOnAction(_ -> {
+                    if (checkbox.isSelected()) {
+                        allCheckbox.setSelected(false);
+                        selectedEquipment.remove("All");
+                        if (!selectedEquipment.contains(checkbox.getText())) selectedEquipment.add(checkbox.getText());
+                    } else {
+                        selectedEquipment.remove(checkbox.getText());
+                    }
+                });
+            }
+        }
+    }
+
+    private static GridPane createBodyPartsSelectorGridPane(List<String> selectedBodyParts) {
         GridPane bodyPartsGridPane = new GridPane();
         bodyPartsGridPane.setHgap(20);
         bodyPartsGridPane.setVgap(20);
         bodyPartsGridPane.setAlignment(Pos.CENTER);
 
         List<ToggleButton> bodyPartToggleButtons = new ArrayList<>();
-
         int columns = 0;
         int rows = 0;
+
         for (BodyPart bodyPart : BodyPart.values()) {
-            Image bodyPartImage = new Image(Objects.requireNonNull(MuscleSelectionView.class.getResourceAsStream("/bodyparts/" +bodyPart.toString()+ ".png")));
-            ImageView bodyPartImageView = new ImageView(bodyPartImage);
-            bodyPartImageView.setFitHeight(60);
-            bodyPartImageView.setFitWidth((double) 200 /2);
-            bodyPartImageView.setPreserveRatio(true);
-
-            Label bodyPartName = new Label(bodyPart.toString());
-            bodyPartName.setMinWidth((double) 200 /3);
-            bodyPartName.setAlignment(Pos.CENTER);
-
-            imageAndTextButtons = new HBox(bodyPartImageView, bodyPartName);
-            imageAndTextButtons.setAlignment(Pos.CENTER);
-            imageAndTextButtons.setSpacing(10);
-
-            ToggleButton bodyPartToggleButton = new ToggleButton();
-            bodyPartToggleButton.setGraphic(imageAndTextButtons);
-            bodyPartToggleButton.setMinSize(200,50);
+            ToggleButton bodyPartToggleButton = createBodyPartToggleButton(bodyPart);
             bodyPartToggleButtons.add(bodyPartToggleButton);
             bodyPartsGridPane.add(bodyPartToggleButton, columns, rows);
 
             columns++;
-            if(columns > 1) {
+            if (columns > 1) {
                 columns = 0;
                 rows++;
             }
         }
 
-        Button submitButton = new Button("Submit");
-        Button backButton = new Button("Back");
-
-        TextField minutesInputField = new TextField();
-
-        submitButton.setPrefSize(200,50);
-        backButton.setPrefSize(200,50);
-
-        minutesInputField.setPrefSize(200, 50);
-        minutesInputField.setMaxSize(200, 50);
-        minutesInputField.setPromptText("Input how many minutes to workout");
-
-        VBox.setMargin(submitButton, new Insets(50, 0, 0, 0));
-
-        inputAndEquipBox.getChildren().addAll(minutesInputField,equipmentSelectorPane);
-        inputAndEquipBox.setSpacing(20);
-        inputAndEquipBox.setAlignment(Pos.CENTER);
-
-        mainVBox.getChildren().add(bodyPartsGridPane);
-        mainVBox.getChildren().add(inputAndEquipBox);
-        mainVBox.getChildren().add(submitButton);
-        mainVBox.getChildren().add(backButton);
-
-
-        submitButton.setOnAction(_ -> {
-            if(minutesInputField.getText() == null || minutesInputField.getText().isEmpty() || !minutesInputField.getText().matches("\\d+")) {
-                System.out.println("Invalid input");
-                return;
-            }
-            int timeInSeconds = Integer.parseInt(minutesInputField.getText());
-            for(ToggleButton toggleButton : bodyPartToggleButtons) {
-                if (toggleButton.isSelected()) {
-                    selectedBodyParts.add(toggleButton.getText());
-                    WorkoutAlgorithm.createWorkoutFromExercises(selectedBodyParts, dislikedBodyParts, selectedEquipment, timeInSeconds);
-                }
-            }
+        bodyPartToggleButtons.forEach(toggleButton -> {
+            toggleButton.setOnAction(e -> {
+                if (toggleButton.isSelected()) selectedBodyParts.add(toggleButton.getText());
+                else selectedBodyParts.remove(toggleButton.getText());
+            });
         });
 
-        backButton.setOnAction(_ -> {
-            ViewController.setScene(HomeScreenView.createScene());
-        });
+        return bodyPartsGridPane;
+    }
 
-        mainVBox.setSpacing(20);
-        mainVBox.setAlignment(Pos.CENTER);
+    private static ToggleButton createBodyPartToggleButton(BodyPart bodyPart) {
+        Image bodyPartImage = new Image(Objects.requireNonNull(MuscleSelectionView.class.getResourceAsStream("/bodyparts/" + bodyPart.toString() + ".png")));
+        ImageView bodyPartImageView = new ImageView(bodyPartImage);
+        bodyPartImageView.setFitHeight(60);
+        bodyPartImageView.setFitWidth(100);
+        bodyPartImageView.setPreserveRatio(true);
 
-        return new Scene(mainVBox, 1000,800);
+        Label bodyPartName = new Label(bodyPart.toString());
+        bodyPartName.setMinWidth(67);
+        bodyPartName.setAlignment(Pos.CENTER);
+
+        HBox imageAndTextButtons = new HBox(10, bodyPartImageView, bodyPartName);
+        imageAndTextButtons.setAlignment(Pos.CENTER);
+
+        ToggleButton bodyPartToggleButton = new ToggleButton();
+        bodyPartToggleButton.setGraphic(imageAndTextButtons);
+        bodyPartToggleButton.setMinSize(200, 50);
+
+        return bodyPartToggleButton;
+    }
+
+    private static void createSubmitButtonFunctionality(List<String> selectedBodyParts, List<String> dislikedBodyParts, List<String> selectedEquipment, TextField minutesInputField) {
+        if (minutesInputField.getText() == null || minutesInputField.getText().isEmpty() || !minutesInputField.getText().matches("\\d+")) {
+            System.out.println("Invalid input");
+            return;
+        }
+        int timeInMinutes = Integer.parseInt(minutesInputField.getText());
+        WorkoutAlgorithm.createWorkoutFromExercises(selectedBodyParts, dislikedBodyParts, selectedEquipment, timeInMinutes);
     }
 }
