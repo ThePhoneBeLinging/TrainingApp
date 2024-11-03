19c3a01eabe4e819763fa774ff2441f70159ecb0
eSavino <s235096@dtu.dk>
2024-11-03 14:10:58 +0100

    New workout now navigates to muscle selection view

--- src/main/java/Group15/View/HomeScreenView.java
+++ src/main/java/Group15/View/HomeScreenView.java
@@ -11,7 +11,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HomeScreenView
