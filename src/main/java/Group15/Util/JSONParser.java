a5a76f5e46007b25897ba5be41115c5b9c346ee7
Frederik Bastrup <s235117@dtu.dk>
2024-11-04 19:22:30 +0100

    Copied new JSON parser in here

--- src/main/java/Group15/Util/JSONParser.java
+++ src/main/java/Group15/Util/JSONParser.java
@@ -3,33 +3,31 @@ package Group15.Util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class JSONParser
-{
+public class JSONParser {
 
-    public static <T> void saveObjectsAsJSON(String filePath, T[] objectsToSave)
-    {
+    public static <T> void saveObjectsAsJSON(String filePath, T[] objectsToSave) {
         ObjectMapper jsonMapper = new ObjectMapper();
-        try
-        {
-            jsonMapper.writeValue(new File(filePath), objectsToSave);
-        }
-        catch (IOException e)
-        {
+        try {
+            File file = new File(filePath);
+            jsonMapper.writeValue(file, objectsToSave);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static <T> T[] loadObjectsFromJSON(String filePath, Class<T[]> objectType)
-    {
+    public static <T> T[] loadObjectsFromJSON(String filePath, Class<T[]> objectType) {
         ObjectMapper jsonMapper = new ObjectMapper();
-        try
-        {
+        try {
+            File file = new File(filePath);
+            if (file.createNewFile()) {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write("[{}]".getBytes());
+            }
             return jsonMapper.readValue(new File(filePath), objectType);
-        }
-        catch (IOException e)
-        {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
