a6ae31e798f14924f8dddc4ef69deee0761e39ba
Elias Aggergaard Larsen <elias@takefive.dk>
2024-11-07 10:23:39 +0100

    Added Jupiter to file

--- src/test/java/ExerciseUtilsTest.java
+++ src/test/java/ExerciseUtilsTest.java
@@ -2,7 +2,7 @@ import Group15.Model.BodyPart;
 import Group15.Model.Equipment;
 import Group15.Model.Exercise;
 import Group15.Util.ExerciseUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Collections;
@@ -10,17 +10,20 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ExerciseUtilsTest {
+public class ExerciseUtilsTest
+{
 
     Exercise pushup = new Exercise("Pushup", "Push your body up and down", "Strength", Collections.singletonList(BodyPart.Chest), Collections.singletonList(Equipment.Bodyweight), "Easy", "None", 3000);
 
     @BeforeEach
-    public void setUp() {
+    public void setUp()
+    {
         ExerciseUtils.init();
     }
 
     @Test
-    public void testInit() {
+    public void testInit()
+    {
         List<Exercise> likedExercises = ExerciseUtils.getLikedExercises();
         List<Exercise> dislikedExercises = ExerciseUtils.getDislikedExercises();
         assertNotNull(likedExercises);
@@ -28,35 +31,40 @@ public class ExerciseUtilsTest {
     }
 
     @Test
-    public void testLikeExercise() {
+    public void testLikeExercise()
+    {
         ExerciseUtils.likeExercisePressed(pushup);
         assertTrue(ExerciseUtils.getLikedExercises().contains(pushup));
         assertFalse(ExerciseUtils.getDislikedExercises().contains(pushup));
     }
 
     @Test
-    public void testDislikeExercise() {
+    public void testDislikeExercise()
+    {
         ExerciseUtils.dislikeExercisePressed(pushup);
         assertTrue(ExerciseUtils.getDislikedExercises().contains(pushup));
         assertFalse(ExerciseUtils.getLikedExercises().contains(pushup));
     }
 
     @Test
-    public void testGetLikedExercises() {
+    public void testGetLikedExercises()
+    {
         ExerciseUtils.likeExercisePressed(pushup);
         List<Exercise> likedExercises = ExerciseUtils.getLikedExercises();
         assertTrue(likedExercises.contains(pushup));
     }
 
     @Test
-    public void testGetDislikedExercises() {
+    public void testGetDislikedExercises()
+    {
         ExerciseUtils.dislikeExercisePressed(pushup);
         List<Exercise> dislikedExercises = ExerciseUtils.getDislikedExercises();
         assertTrue(dislikedExercises.contains(pushup));
     }
 
     @Test
-    public void testWriteToFile() {
+    public void testWriteToFile()
+    {
         ExerciseUtils.likeExercisePressed(pushup);
         assertTrue(ExerciseUtils.getLikedExercises().contains(pushup));
     }
