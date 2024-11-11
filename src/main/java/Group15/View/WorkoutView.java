fc03d694444cbf9d0c9417a311bb9e0ba2106d1a
Mads Hartwich <s235102@dtu.dk>
2024-11-11 01:03:39 +0100

    added navigation from WorkoutView back to MuscleSelector and form WorkoutView to EditWorkoutView

--- src/main/java/Group15/View/WorkoutView.java
+++ src/main/java/Group15/View/WorkoutView.java
@@ -121,9 +121,8 @@ public class WorkoutView {
             newButton.setPrefSize(200, 50);
             newButton.setOnAction(_ -> {
                 switch (button){
-                    case "Back" -> ViewController.setScene(HomeScreenView.createScene());
-                    case "Edit Workout" -> {
-                    }
+                    case "Back" -> ViewController.setScene(MuscleSelectionView.createMuscleSelectorScene());
+                    case "Edit Workout" -> ViewController.setScene(EditWorkoutView.createScene(Workout));
                     case "Save" -> {
                         try {
                             WorkoutPdfGenerator.saveWorkoutAsPdf(Workout, "workout.pdf");
